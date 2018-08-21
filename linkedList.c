db944b955d9730d9ee919bea6560e55d573d3601
Haoyu Wang <wanghaoyu171@mails.ucas.ac.cn>
2018-08-22 00:21:35 +0800

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -1,22 +1,34 @@
+//
+//  linkedList.c
+//  linkedList
+//
+//  Created by 王浩宇 on 2018/8/21.
+//  Copyright © 2018 UCAS Developers. All rights reserved.
+//
 
 #include <stdio.h>
 #include "linkedList.h"
 
 int main(void) {
-
+    
     int count;
     printf("Please enter the number of nodes you want to create:");
     scanf("%d", &count);
     struct Node * pHead = NULL;
+    
     pHead = createLinkedList(count);
     if (!(pHead -> ptr)) {printf("creation failed!\n");}
     else {printf("creation succeeded!\n");}
+    
     int len = getLen(pHead);
     printf("The number of nodes is:%d\n", len);
+    
     if (input(pHead)) {printf("input succeeded!\n");}
     else {printf("input failed!\n");}
+    
     if (showData(pHead)) {printf("show data succeeded!\n");}
     else {printf("show data failed!\n");}
+    
     double insertData;
     int pos;
     printf("Please enter the data you want to insert:");
@@ -25,26 +37,47 @@ int main(void) {
     scanf("%d", &pos);
     if (insert(pHead, pos, insertData)) {printf("insert succeeded!\n");}
     else {printf("insert failed!\n");}
+    
     len = getLen(pHead);
     printf("Now the number of nodes is:%d\n", len);
+    
     if (showData(pHead)) {printf("show data succeeded!\n");}
     else {printf("show data failed!\n");}
+    
     if (sort(pHead)) {printf("sort succeeded!\n");}
     else {printf("sort failed!\n");}
+    
     if (showData(pHead)) {printf("show sorted data succeeded!\n");}
     else {printf("show sorted data failed!\n");}
+    
+    double newData;
+    printf("Please enter the position of the data you want to change(start from 0):");
+    scanf("%d", &pos);
+    printf("Please enter the new data:");
+    scanf("%lf", &newData);
+    if (changeData(pHead, pos, newData)) {printf("change data succeeded!\n");}
+    else {printf("change data failed!\n");}
+    
+    if (showData(pHead)) {printf("show changed data succeeded!\n");}
+    else {printf("show changed data failed!\n");}
+    
     printf("Please enter the position of the data you want to get(start from 0):");
     scanf("%d", &pos);
     printf("The data is:%g\n", getData(pHead, pos));
+    
     printf("Please enter the position of the node you want to remove(start from 0):");
     scanf("%d", &pos);
     if (removeData(pHead, pos)) {printf("remove data succeeded!\n");}
     else {printf("remove data failed!\n");}
+    
     len = getLen(pHead);
     printf("Now the number of nodes is:%d\n", len);
+    
     if (showData(pHead)) {printf("show removed data succeeded!\n");}
     else {printf("show removed data failed!\n");}
+    
     if (removeLinkedList(pHead)) {printf("remove linkedlist succeeded!\n");}
     else {printf("remove linkedlist failed!\n");}
+    
     return 0;
 }
